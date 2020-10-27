66553911d73ae736cfc57d43a0f6257867b6d5a2
Ubuntu <ubuntu@ip-172-31-39-35.us-east-2.compute.internal>
2020-10-27 08:52:19 +0000

    finetuned BERT Base to run c 6k seconds on g3s

--- language_model/tensorflow/bert/run_and_time.sh
+++ language_model/tensorflow/bert/run_and_time.sh
@@ -9,10 +9,10 @@ python run_pretraining.py \
   --do_train \
   --eval_batch_size=8 \
   --learning_rate=4e-05 \
-  --iterations_per_loop=1 \
+  --iterations_per_loop=10 \
   --max_predictions_per_seq=76 \
   --max_seq_length=512 \
-  --num_train_steps=5000 \
+  --num_train_steps=15000 \
   --num_warmup_steps=15 \
   --optimizer=lamb \
   --save_checkpoints_steps=5000 \
